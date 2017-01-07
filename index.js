23f06555517b734d95ec4541c468763caf330ffc
Russell Vea <russellbvea@gmail.com>
2017-01-07 13:45:44 -1000

    Apologize script

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello, World')
+const record = require('node-record-lpcm16')
+const stream = require('stream')
+const { Detector, Models } = require('snowboy')
+
+const models = new Models()
+
+models.add({
+  file: 'travis.pmdl',
+  sensitivity: '0.5',
+  hotword: 'travis'
+})
+
+const detector = new Detector({
+  resource: "resources/common.res",
+  models: models,
+  audioGain: 2.0
+});
+
+detector.on('hotword', function (index, hotword) {
+  console.log('hotword', index, hotword);
+});
+
+const mic = record.start({
+  threshold: 0,
+  verbose: false
+});
+
+mic.pipe(detector)
